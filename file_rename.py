a28c25f2ff6985009375b9430fc82425fc4b050f
caesaranyogu <117361582+caesaranyogu@users.noreply.github.com>
2025-08-26 23:21:30 -0400

    Succesful file_rename.py
    Had to make sure paths set up correctly for computer to find folder and files

--- file_rename.py
+++ file_rename.py
@@ -1,1 +1,14 @@
+import os
 
+# Folder where files are
+folder = "/Users/juliusyusako/PycharmProjects/pythonfilerenamer/test_files"
+
+# Loop through files in the folder
+for i, filename in enumerate(os.listdir(folder), start=1):
+    # Reset new_name text to desired name
+    new_name = f"report{i}.txt"
+    file_path = os.path.join(folder, filename)  # full path to existing file
+    new_path = os.path.join(folder, new_name)  # full path to new name
+
+    os.rename(file_path, new_path)
+    print(f"Renamed {filename} → {new_name}")
